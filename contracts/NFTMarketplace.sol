1bced024ff58f12e006faf2629e083762dbad759
Haimanotgg <Haimanotgbb@gmail.com>
2023-05-28 20:01:35 +0300

    first commit

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -60,6 +60,37 @@ contract NFTMarketplace is ERC721URIStorage {
     //CREATE NFT TOKEN FUNCTION
 
     function createToken( string memory tokenURI, uint256 price) public payable returns (uint256) {
-        
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(msgsender, newTokenId);
+        _setTokenURI(tokenId, _tokenURI);
+
+        createMarketItem(newTokenId, price);
+
+        return newTokenId;
+    }
+
+    function createMarketItem(uint256 tokenId, uint256 price) private{
+        require(price ? 0, "Price must be at least 1");
+        require(msg.value == listingPrice, "Price must be equal to listing");
+
+        idMarketItem[tokenId] = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false,
+        );
+
+        _transfer(msg.sender, address(this), tokenId);
+
+        emit  idMarketItemCreated(tokenId, msg.sender, address(this), price, false)
+    }
+
+    //FUNCTION FOR RESALE TOKEN
+    function resellToken(uint256 tokenId, uint256 price) public payable{
+        require (idMarketItem[tokenId].owner == msg.sender);
     }
 }
